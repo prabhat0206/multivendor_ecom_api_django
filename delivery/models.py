bdf86daffd6309fbbd6a765227d8b9aebdb5d0d0
Prabhat Ranjan <www.encodedprogrammer@gmail.com>
2022-06-20 13:19:52 +0530

    almost ready

--- delivery/models.py
+++ delivery/models.py
@@ -8,8 +8,11 @@ from users.models import User
 class DeliveryBoy(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=300, default="email@example.com")
     ph_number = PhoneNumberField()
     password = models.CharField(max_length=200)
     is_offline = models.BooleanField(default=True)
     added_by = models.ForeignKey(User, on_delete=models.CASCADE)
     joined_at = models.DateTimeField(auto_now_add=True)
+    address = models.TextField()
+    profile_pic = models.ImageField(upload_to="delivery_boy", null=True, blank=True)
